67f4ccb1ec739ab4c3092bfdad3e66e505c54776
Mehar-Aziz <meharaziz.1614>
2024-07-12 21:06:29 +0500

    exercise

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-num_char = len(input("What is your name? "))
-new_num_char = str(num_char)# convert to
-print("Your name has " + new_num_char + " Characters.")
+#Input two number and result should add two numbers digits
+two_digit_num = input("Enter 2 digit number: ")
+print(int(two_digit_num[0])+int(two_digit_num[1]))
 
-a = float(123)
-print(type(a))
-
-print(70 + float("100.5")) #170.5
-print(str(70) + str(100)) #70100
-print(float("100"))#100.0
+#22
+#answer=4
