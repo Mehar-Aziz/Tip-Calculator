51a2b9fef176e6429ec2ba903b695b99099e3f12
Mehar-Aziz <meharaziz.1614>
2024-07-12 21:56:46 +0500

    Arithmetic operators

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#Input two number and result should add two numbers digits
-two_digit_num = input("Enter 2 digit number: ")
-print(int(two_digit_num[0])+int(two_digit_num[1]))
-
-#22
-#answer=4
+score = 3
+score *= 2
+print(score)
