866bd8bbbf214f1d4d5cb724f2f4975eaceb35f8
Mehar-Aziz <meharaziz.1614>
2024-07-12 20:52:02 +0500

    type function

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-#Data Types
-#String
-print("Hello"[0]) #print first characters
-print("Hello"[4]) #print 4th characters
-
-#Integer
-
-print(123 + 345)
-
-123_245_890  #for large numbers, wrtite underscore
-
-#float
-
-3.14159 # decimal
-
-#boolean
-
-True
-False
+num_char = len(input("What is your name? "))
+print(type(num_char))
