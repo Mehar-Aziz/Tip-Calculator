ece0f737b423e292085d04f69b46a56333d8de76
Mehar-Aziz <meharaziz.1614>
2024-07-12 21:00:47 +0500

    type casting

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 num_char = len(input("What is your name? "))
-print(type(num_char))
+new_num_char = str(num_char)# convert to
+print("Your name has " + new_num_char + " Characters.")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5")) #170.5
+print(str(70) + str(100)) #70100
+print(float("100"))#100.0
